a25d6eaf28e680cfbad33e364563ef8dc888ba65
emorera07 <emmanuel.morera@ucr.ac.cr>
2021-06-26 00:18:10 -0600

    update

--- app/src/main/java/com/example/carssoundsapp/MainActivity.java
+++ app/src/main/java/com/example/carssoundsapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.carssoundsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -10,7 +11,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer mp;
+    MediaPlayer mp1, mp2, mp3, mp4, mp5, mp6;
     Button bt_Aceleracion;
     Button bt_bus_freno;
     Button bt_carrera;
@@ -25,51 +26,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bt_Aceleracion = (Button)findViewById(R.id.bt_Aceleracion);
-        mp = MediaPlayer.create(this, R.raw.aceleracion_1);
+
+        mp1 = MediaPlayer.create(this, R.raw.aceleracion_1);
         bt_Aceleracion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.start();
+                mp1.start();
             }
         });
         bt_bus_freno = (Button)findViewById(R.id.bt_bus_freno);
-        mp = MediaPlayer.create(this, R.raw.autobus_frenando);
+        mp2 = MediaPlayer.create(this, R.raw.autobus_frenando);
         bt_bus_freno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.start();
+                mp2.start();
             }
         });
         bt_auto_fant = (Button)findViewById(R.id.bt_auto_fant);
-        mp = MediaPlayer.create(this, R.raw.auto_fantastico);
+        mp3 = MediaPlayer.create(this, R.raw.auto_fantastico);
         bt_auto_fant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.start();
+                mp3.start();
             }
         });
         bt_camion_helado = (Button)findViewById(R.id.bt_camion_helado);
-        mp = MediaPlayer.create(this, R.raw.camion_helado);
+        mp4 = MediaPlayer.create(this, R.raw.camion_helado);
         bt_camion_helado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.start();
+                mp4.start();
             }
         });
         bt_carrera = (Button)findViewById(R.id.bt_carrera);
-        mp = MediaPlayer.create(this, R.raw.auto_carrera);
+        mp5 = MediaPlayer.create(this, R.raw.auto_carrera);
         bt_carrera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.start();
+                mp5.start();
             }
         });
         bt_frenar = (Button)findViewById(R.id.bt_frenar);
-        mp = MediaPlayer.create(this, R.raw.frenar);
+        mp6 = MediaPlayer.create(this, R.raw.frenar);
         bt_frenar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.start();
+                mp6.start();
             }
         });
     }
